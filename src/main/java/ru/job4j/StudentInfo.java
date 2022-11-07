739d774eb8ea95aed6611cb6568f12f0cb763bf4
Lesa-89 <lesa-89@mail.ru>
2022-11-07 22:33:16 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Larichev");
+        System.out.println("26.12.1989");
+    }
+}
