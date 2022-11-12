7e65a375f0bbf8d7ed3677d280a705aa99600c38
Lesa-89 <lesa-89@mail.ru>
2022-11-13 00:20:01 +0300

    4.3.2. Сокращенный оператор || [#333784]

---
+++ src/main/java/ru/job4j/condition/OR.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class OR {
+    public static void main(String[] args) {
+        boolean first = true;
+        boolean second = true;
+        boolean join = first || second;
+        System.out.println(join);
+    }
+}
