4612055ec7e7ebdb74f6ab4242824f084479d521
Lesa-89 <lesa-89@mail.ru>
2022-11-14 23:05:46 +0300

    5.5. Простое число [#171694 #360878]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -15,16 +15,4 @@ public class CheckPrimeNumber {
 
         return prime;
     }
-
-    public static void main(String[] args) {
-        System.out.println(check(1));
-        System.out.println(check(2));
-        System.out.println(check(3));
-        System.out.println(check(4));
-        System.out.println(check(5));
-        System.out.println(check(6));
-        System.out.println(check(7));
-        System.out.println(check(8));
-        System.out.println(check(9));
-    }
 }
