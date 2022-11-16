23fb7e86c73327515031af65cf280af3a8995b4a
Lesa-89 <lesa-89@mail.ru>
2022-11-16 18:52:16 +0300

    6.6.3. Двухмерный массив. If. [#306727 #361227]

--- src/main/java/ru/job4j/array/MatrixFind.java
+++ src/main/java/ru/job4j/array/MatrixFind.java
@@ -5,8 +5,7 @@ public class MatrixFind {
         System.out.println("Find el : " + el);
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                int val = array[row][cell];
-                if (val == el) {
+                if (array[row][cell] == el) {
                     System.out.println("row : " + row + ", cell : " + cell);
                 }
             }
