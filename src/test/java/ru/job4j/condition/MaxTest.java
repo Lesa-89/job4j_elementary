5940d691ccc05d3eecb09a89283230322d1a9863
Computer <lesa-89@mail.ru>
2022-11-26 20:29:42 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -39,4 +39,65 @@ public class MaxTest {
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax5To5To6Then6() {
+        int first = 5;
+        int second = 5;
+        int third = 6;
+        int result = Max.max(first, second, third);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax7To5To6Then6() {
+        int first = 7;
+        int second = 5;
+        int third = 6;
+        int result = Max.max(first, second, third);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax5To7To6Then6() {
+        int first = 5;
+        int second = 7;
+        int third = 6;
+        int result = Max.max(first, second, third);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax5To5To5Then5() {
+        int first = 5;
+        int second = 5;
+        int third = 5;
+        int result = Max.max(first, second, third);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax5To5To6To6Then6() {
+        int first = 5;
+        int second = 5;
+        int third = 6;
+        int fourth = 6;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax8To5To6To6Then6() {
+        int first = 8;
+        int second = 5;
+        int third = 6;
+        int fourth = 6;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+    public void whenMax5To5To5To5Then5() {
+        int first = 5;
+        int second = 5;
+        int third = 5;
+        int fourth = 5;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
 }
